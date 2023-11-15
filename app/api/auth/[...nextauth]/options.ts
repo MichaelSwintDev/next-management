c221c9318ccbd4f9eaaefd2c8936ff0564010171
Michael Swint <michaelswintdev@gmail.com>
2023-11-15 17:26:16 -0500

    init auth

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,17 @@
+import {NextAuthOptions} from "next-auth";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import prisma from "@/lib/db";
+import {PrismaClient} from "@prisma/client";
+import {Adapter} from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import { env } from "@/lib/env";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
