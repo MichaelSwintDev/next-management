70f4a09208714aa44c7b4e04192cfb82828d8f3c
Michael Swint <michaelswintdev@gmail.com>
2023-11-17 21:27:51 -0500

    navbar added

---
+++ app/Navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import UserMenuButton from "@/app/Navbar/UserMenuButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+
+export default async function Navbar() {
+  const session = await getServerSession(authOptions);
+  return (
+    <div className=" bg-base-300 ">
+      <div className="navbar m-auto max-w-7xl flex-col gap-2 sm:flex-row">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+              />
+            </svg>
+            Next Rentals
+          </Link>
+        </div>
+        <UserMenuButton session={session}></UserMenuButton>
+      </div>
+    </div>
+  );
+}
